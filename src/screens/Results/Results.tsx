a700d01c07d55fe1825eb287b90040337f79bf7e
Daniel Gutierrez <danielgudu22@gmail.com>
2021-11-25 17:33:32 -0500

    define circle to show result

--- src/screens/Results/Results.tsx
+++ src/screens/Results/Results.tsx
@@ -1,5 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
+import { useEffect } from "react";
 import {
   Dimensions,
   Image,
@@ -9,23 +10,24 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import { PieChart } from "react-native-chart-kit";
+import { PieChart, ProgressChart } from "react-native-chart-kit";
 import ImageLayout from "../../common/layouts/ImageLayout";
 import { RootStackParamList } from "../../navigation/types";
 import Color from "../../res/constants/colors";
 import Font from "../../res/constants/fonts";
 import FontSize from "../../res/constants/fontSizes";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
+import { Circle } from "react-native-svg";
 
 const { height, width } = Dimensions.get("window");
 
 const chartConfig = {
-  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-  // backgroundGradientFrom: "#1E2923",
-  // backgroundGradientFromOpacity: 0,
-  // backgroundGradientTo: "#08130D",
-  // backgroundGradientToOpacity: 0.5,
-  // strokeWidth: 3, // optional, default 3
-  // barPercentage: 0.5,
+  color: (opacity = 1) => `rgb(0, 144, 208, ${opacity})`,
+  backgroundGradientFrom: "#000000",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#000000",
+  backgroundGradientToOpacity: 0,
+  barPercentage: 0.5,
 };
 
 const Results = ({
@@ -36,17 +38,20 @@ const Results = ({
     navigation.navigate("RegisterPatient");
   };
 
-  const data = route.params?.questions
-    .filter((question) => question.isYes)
-    .map((question, index) => {
-      return {
-        name: `Pregunta ${index + 1}: ${question.name}`,
-        weight: question.weight,
-        color: question.color,
-        legendFontColor: Color.TEXT_QUESTION,
-        legendFontSize: FontSize.ANSWER,
-      };
-    });
+  const dataWeight = () => {
+    let tempData = 0;
+    route.params?.questions
+      .filter((question) => question.isYes)
+      .map((question, index) => {
+        tempData += question.weight;
+      });
+    return tempData;
+  };
+
+  const data = {
+    data: [0, 0, dataWeight() / 100],
+    color: [Color.PRIMARY],
+  };
 
   return (
     <ImageLayout>
@@ -58,39 +63,35 @@ const Results = ({
           {data && (
             <>
               <View style={styles.questions}>
-                <PieChart
+                <AnimatedCircularProgress
+                  size={224}
+                  width={10}
+                  fill={dataWeight()}
+                  fillLineCap="round"
+                  tintColor={Color.PRIMARY}
+                  backgroundColor={Color.SECONDARY}
+                  backgroundWidth={5}
+                  padding={10}
+                />
+                {/* <ProgressChart
                   data={data}
                   width={width}
                   height={280}
                   chartConfig={chartConfig}
-                  accessor={"weight"}
-                  backgroundColor={"transparent"}
-                  paddingLeft={"0"}
                   center={[width / 4, 0]}
                   hasLegend={false}
-                />
+                  hideLegend={true}
+                  strokeWidth={15}
+                />*/}
                 <View style={styles.sum}>
                   <Text style={styles.sumText}>
-                    {data.reduce((partial_sum, a) => partial_sum + a.weight, 0)}
-                    %
+                    {data.data.reduce(
+                      (partial_sum, a) => (partial_sum + a) * 100,
+                      0
+                    )}
+                    <Text style={{ fontSize: 30, marginBottom: 30 }}>%</Text>
                   </Text>
-                  <Text style={styles.totalText}>Total</Text>
                 </View>
-              </View>
-              <View style={styles.answersGroup}>
-                {data.map((answer, index) => (
-                  <View key={index} style={styles.answer}>
-                    <View
-                      style={[styles.number, { backgroundColor: answer.color }]}
-                    >
-                      <Text style={styles.numberText}>
-                        {answer.weight}
-                        <Text style={styles.percentage}>%</Text>
-                      </Text>
-                    </View>
-                    <Text style={styles.answersText}>{answer.name}</Text>
-                  </View>
-                ))}
               </View>
             </>
           )}
@@ -129,7 +130,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   sum: {
-    backgroundColor: Color.TEXT_SECONDARY,
+    backgroundColor: Color.PRIMARY,
     borderRadius: 100,
     height: 175,
     width: 175,
@@ -140,13 +141,13 @@ const styles = StyleSheet.create({
   sumText: {
     fontFamily: Font.BOLD,
     fontSize: FontSize.TOTAL,
-    color: Color.TEXT_PRIMARY,
+    color: Color.TEXT_SECONDARY,
     lineHeight: 60,
   },
   totalText: {
     fontFamily: Font.REGULAR,
     fontSize: FontSize.ANSWER,
-    color: Color.TEXT_PRIMARY,
+    color: Color.TEXT_SECONDARY,
   },
   answersGroup: {
     alignSelf: "flex-start",
@@ -185,6 +186,7 @@ const styles = StyleSheet.create({
     height: height * 0.05,
     marginTop: 30,
     marginBottom: 10,
+    alignSelf: "center",
   },
   footerText: {
     textAlign: "center",
